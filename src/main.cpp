c8ec8b80f31b87637fb2e889065454f09abcb4c7
MoonStdio <juxiupan@163.com>
2025-07-11 00:30:55 +0800

    改变main.cpp测试分支保存

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "spdlog/spdlog.h"
 int main(){
     spdlog::set_level(spdlog::level::trace); // 设置spdlog的日志级别为trace
-
+    spdlog::warn("this is list branch");
     engine::core::Game game;
     game.run();
 
